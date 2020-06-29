273dc5362dc3d97fcbf0daaaae157d7d98a14d65
Forrest <Forrestmillerj@gmail.com>
2020-06-29 16:57:18 -0500

    setup template parsing and routes for '/'

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./templates/*"))
+}
+
 func main() {
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(res http.ResponseWriter, req *http.Request) {
+
+	tpl.ExecuteTemplate(res, "index.html", nil)
 
 }
