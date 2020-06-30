0059aa4748cfbe23b662cd94ac8c4d9e9ac4bfc7
Forrest <Forrestmillerj@gmail.com>
2020-06-30 14:13:41 -0500

    updated to create a new file from POST route in public/pics

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
-	"text/template"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -22,8 +27,36 @@ func main() {
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
 	c := getCookie(res, req)
+	// process form submission
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		// create sha for file name
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		// create new file
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copy
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		// add filename to this users's cookie
+		c = appendValues(res, c, fname)
+	}
 	xs := strings.Split(c.Value, "|")
-
 	tpl.ExecuteTemplate(res, "index.gohtml", xs)
 
 }
@@ -34,30 +67,18 @@ func getCookie(res http.ResponseWriter, req *http.Request) *http.Cookie {
 		sID, _ := uuid.NewV4()
 		c = &http.Cookie{
 			Name:  "session",
-			Value: sID.String() + "|" + "sunset.jpg" + "|" + "meadow.jpg" + "|" + "beach.jpg",
+			Value: sID.String(),
 		}
 		http.SetCookie(res, c)
 	}
 	return c
 }
 
-func appendValues(res http.ResponseWriter, c *http.Cookie) *http.Cookie {
-	// Values
-	p1 := "sunset.jpg"
-	p2 := "meadow.jpg"
-	p3 := "beach.jpg"
-	// Append
+func appendValues(res http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
 	s := c.Value
-	if !strings.Contains(s, p1) {
-		s += "|" + p1
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
 	}
-	if !strings.Contains(s, p2) {
-		s += "|" + p2
-	}
-	if !strings.Contains(s, p3) {
-		s += "|" + p3
-	}
-	// Set
 	c.Value = s
 	http.SetCookie(res, c)
 	return c
