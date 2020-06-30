8c7390f28c72b1cbfcca9006a72b6d6c04912727
Forrest <Forrestmillerj@gmail.com>
2020-06-30 12:08:22 -0500

    added appendValue() to append file names to cookie value

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"text/template"
 
 	uuid "github.com/satori/go.uuid"
@@ -21,8 +22,9 @@ func main() {
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
 	c := getCookie(res, req)
+	xs := strings.Split(c.Value, "|")
 
-	tpl.ExecuteTemplate(res, "index.gohtml", c.Value)
+	tpl.ExecuteTemplate(res, "index.gohtml", xs)
 
 }
 
@@ -32,9 +34,31 @@ func getCookie(res http.ResponseWriter, req *http.Request) *http.Cookie {
 		sID, _ := uuid.NewV4()
 		c = &http.Cookie{
 			Name:  "session",
-			Value: sID.String(),
+			Value: sID.String() + "|" + "sunset.jpg" + "|" + "meadow.jpg" + "|" + "beach.jpg",
 		}
 		http.SetCookie(res, c)
 	}
+	return c
+}
+
+func appendValues(res http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	// Values
+	p1 := "sunset.jpg"
+	p2 := "meadow.jpg"
+	p3 := "beach.jpg"
+	// Append
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	// Set
+	c.Value = s
+	http.SetCookie(res, c)
 	return c
 }
